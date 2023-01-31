22a585892adfb0ba41e2813e3bfcf7e6837cbe20
Chus <22170934@live.uem.es>
2023-01-31 22:06:50 +0100

    Crear php y corregir index
    Creacion de archivos php para acceso a BBDD. Algunas correcciones en el index

---
+++ dist/php/connect_inmuebles populares_db.php
@@ -0,0 +1,43 @@
+<?php
+// Create connection
+$conn = mysqli_connect("credentials.php");
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "SELECT ID, DESCRIPCION, PRECIO, DIMENSION, HABITACIONES, BAÑOS, EMAIL, RESERVADA FROM Inmuebles populares";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  echo '<table>';
+  echo "<tr><td>ID</td>
+  <td>DESCRIPCION</td>
+  <td>PRECIO</td>
+  <td>DIMENSION</td>
+  <td>HABITACIONES</td>
+  <td>BAÑOS</td>
+  <td>EMAIL</td>
+  <td>RESERVADA</td></tr>";
+
+  while($row = mysqli_fetch_assoc($result)) {
+	echo "<tr>";
+    echo 
+    "<td>" . $row["ID"]. "</td>
+     <td>" . $row["DESCRIPCION"]. "</td>
+     <td>" . $row["PRECIO"]. "</td>
+     <td>" . $row["DIMENSION"]. "</td>
+     <td>" . $row["HABITACIONES"]. "</td>
+     <td>" . $row["BAÑOS"]. "</td>
+     <td>" . $row["EMAIL"]. "</td>
+     <td>" . $row["RESERVADA"]."</td>";
+	echo "</tr>";
+	}
+  echo '</table>';
+} else {
+  echo "0 results";
+}
+
+
+echo "cerrando conexión";
+mysqli_close($conn);
